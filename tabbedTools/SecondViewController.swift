7fc8d31d5f04f4e3dbf40637f5deda180a5edb88
superw <sw@swmbp13.local>
2018-12-02 18:14:54 +0100

    Codded the fibonacci number’s logic in SecondViewController.swift: The fibonacci number’s logic is simple: We add the last two numbers of the series and append the result to the end of the array (using .append method), so we must initialize the array, at least, with the first two numbers, that is [0,1] that corresponds to the number 2 of the fibonacci series. To calculate the third: [0,1,(0+1)] = [0,1,1] and the fourth: [0,1,1,(1+1)] = [0,1,1,2]… Note the fibonacci’s series growth quickly! the number 22 of the fibonacci series is already 10946. Using an Int data array, we realize that the maximum number of the Fibonacci series that fits in this type of data is 92 that corresponds to the number Int 7540113804746346429, so 92 will be the highest number that can be selected by user. We need to define and control this maximum number because if we do not we will get a runtime error with value 93 because the result 1,220016042E19 does not fit in an Int data type

--- tabbedTools/SecondViewController.swift
+++ tabbedTools/SecondViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    //variable (array de enteros) que irá guardando los números de fibonacci generados inicializandola con los dos números enteros obligarotios
+    var fibonacci : [Int] = [0,1]
+    //variable para guardar cuantos números de fibonacci quiero generar
+    var fibID = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //según se cargue la vista, es decir, el usuario haga tap en la segunda pestaña se ejecuta este código
+        fibID = 92 //establezco el fibID en el máximo que me es capaz de almacenar en un tipo Int
+        generateFibonacciNumbers() //llamo a la función que me genera los números
     }
+    
+    func generateFibonacciNumbers() {
+        //si el número a generar es menor o igual que uno no tiene sentido generarlos porque ya hemos inicializado con [0,1] el array.
+        //también limitamos por encima de los 92 primeros números por motivos computacioinales no vamos a calcularlos
+        //FYI: El último número de fibonacci que cabe en un tipo Int es el 7540113804746346429 que corresponde con el número 92 de la serie de fibonacci
+        if (fibID <= 1) || (fibID > 92) {
+            print(" >>> ABORTANDO.. FUERA DE RANGO <<< ")
+            return //si fibID no está en el rango permitido simplemente salimos
+        }
+        //a partir de aquí ya podemos trabajar con el fibID para generar números de fibonacci
+        //antes de nada restablecemos la colección
+        fibonacci = [0,1]
+        //vamos recorriendo el array
+        for i in 2...fibID { //desde el número 2 hasta el fibID incluido
+            //para cada elemento agregamos la suma de los dos anteriores
+            fibonacci.append(fibonacci[i-1]+fibonacci[i-2])
+            print("posición \(i): \(fibonacci[i])")
+            //imprimimos el elemento añadido cada vez
+        }
+        //print(fibID)
+        //print(fibonacci)
+    }
+    
 
 
 }
