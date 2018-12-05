7637708d113e09a66fef1c1cf92cdd658279605f
superw <sw@swmbp13.local>
2018-12-05 01:40:30 +0100

    Creating the graphic interface for font’s detail view. Added a new view controller called asdf to show the user how looks the font once was selected and also list all the fonts names of the family. label, picker and textview Connected to control them by code in FontDetailViewController.swift Tags for this update: segue, Show, ViewController, PickerView

---
+++ tabbedTools/FontDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FontDetailViewController.swift
+//  tabbedTools
+//
+//  Created by superw on 05/12/2018.
+//  Copyright © 2018 superw. All rights reserved.
+//
+
+import UIKit
+
+class FontDetailViewController: UIViewController {
+    
+    //variables IBOulet para el control por código de los elementos del interfaz grafico. Necesitamos motificar el contenido de la etiqueta del título y conocer qué fuente ha elegido el usuario en el picker para poder cambiar el texto del text view y configurarlo con la fuente seleccionada
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var pickerFonts: UIPickerView!
+    @IBOutlet weak var textView: UITextView!
+    
+    //esto ocurrirá según se muestre este FontDetailViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("\n >>> ESTAMOS EN LA PESTAÑA: DETALLE DE LA FAMILIA DE LA FUENTE <<< ")
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
